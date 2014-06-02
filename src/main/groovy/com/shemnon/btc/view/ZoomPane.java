effd601965875fec8891f8202afea1f84f1daf54
Danno Ferrin <danno.ferrin@shemnon.com>
2014-06-02 14:43:00 -0600

    Chagne the ZoomPane License to BSD

--- src/main/groovy/com/shemnon/btc/view/ZoomPane.java
+++ src/main/groovy/com/shemnon/btc/view/ZoomPane.java
@@ -1,20 +1,28 @@
 /*
- * Follow the  Bitcoin
- * Copyright (C) 2014  Danno Ferrin
+ * Copyright (c) 2014, Danno Ferrin
+ * All rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package com.shemnon.btc.view;
 
@@ -30,51 +38,51 @@ import javafx.scene.layout.Pane;
 import javafx.scene.transform.Affine;
 
 /**
- * 
+ *
  * Created by shemnon on 3 Mar 2014.
  */
 public class ZoomPane extends Pane {
 
     double scrollScaleFactor = 1.1;
-    
+
     double minZoom = 0.01; // 1:100
     double maxZoom = 10.0; // 10:1
-    
+
     Affine transform;
     Affine workTransform;
     Node zoomNode;
-    
-    
+
+
     DoubleProperty scale = new SimpleDoubleProperty(1.0);
     DoubleProperty tx = new SimpleDoubleProperty(0.0);
     DoubleProperty ty = new SimpleDoubleProperty(0.0);
-    
+
     boolean set = false;
     double lastScale = 1.0;
     double lastTX = 0;
     double lastTY = 0;
     double lastX = 0;
     double lastY = 0;
-    
+
     final Group zoomGroup;
 
     public ZoomPane(Node... nodes) {
         super();
         zoomGroup = new Group(nodes);
         getChildren().addAll(zoomGroup);
-        
+
         transform = new Affine();
         workTransform = new Affine();
-        
+
         getChildren().get(0).getTransforms().setAll(transform);
-        
+
         setOnZoomStarted(e -> start(e.getX(), e.getY()));
         setOnScrollStarted(e -> start(e.getX(), e.getY()));
         setOnMousePressed(e -> start(e.getX(), e.getY()));
         setOnZoomFinished(e -> finish());
         setOnMouseReleased(e -> finish());
         setOnScrollFinished(e -> finish());
-        
+
         setOnZoom(this::zooming);
         setOnScroll(this::scrolling);
         setOnMouseDragged(this::dragging);
@@ -98,14 +106,14 @@ public class ZoomPane extends Pane {
                 scale.get(), 0, tx.get(),
                 0, scale.get(), ty.get());
     }
-    
-    public void zooming(ZoomEvent ze) { 
+
+    public void zooming(ZoomEvent ze) {
         zoom(ze.getTotalZoomFactor(), ze.getX(), ze.getY());
     }
 
     private void zoom(double zoomFactor, double x, double y) {
         if (!set) start(x, y);
-        
+
         double cs = scale.get();
         double netZoom = zoomFactor * cs;
         if (netZoom < minZoom) {
@@ -113,18 +121,18 @@ public class ZoomPane extends Pane {
         } else if (netZoom > maxZoom) {
             zoomFactor = maxZoom / cs;
         }
-        
+
         workTransform.setMxx(lastScale);
         workTransform.setMyy(lastScale);
         workTransform.setTx(lastTX);
         workTransform.setTy(lastTY);
-        
+
         workTransform.prependScale(zoomFactor, zoomFactor, x, y);
-        
+
         scale.set(workTransform.getMxx());
         tx.set(workTransform.getTx());
         ty.set(workTransform.getTy());
-        
+
         writeToTransform();
     }
 
@@ -138,10 +146,10 @@ public class ZoomPane extends Pane {
                 start(se.getX(), se.getY());
             }
             if (se.isInertia()) return; // inertia tends to cause crazy zooms
-    
+
             clickCount = se.getTotalDeltaY() / se.getMultiplierY();
         }
-        zoom(Math.pow(scrollScaleFactor, clickCount), 
+        zoom(Math.pow(scrollScaleFactor, clickCount),
              se.getX(), se.getY());
     }
 
@@ -149,8 +157,8 @@ public class ZoomPane extends Pane {
     public void dragging(MouseEvent me) {
         if (!set) start(me.getX(), me.getY());
         if (!Double.isNaN(lastX) && !Double.isNaN(lastY)) {
-            double dx = me.getX() - lastX; 
-            double dy = me.getY() - lastY; 
+            double dx = me.getX() - lastX;
+            double dy = me.getY() - lastY;
             tx.set(tx.get() + dx);
             ty.set(ty.get() + dy);
         }
@@ -158,42 +166,42 @@ public class ZoomPane extends Pane {
         lastY = me.getY();
         writeToTransform();
     }
-    
+
     public void center() {
         Bounds gb = zoomGroup.getLayoutBounds();
         Bounds tb = getLayoutBounds();
         double scale = this.scale.get();
-        
+
         tx.set((tb.getWidth() - gb.getWidth()*scale) / 2);
         ty.set((tb.getHeight() - gb.getHeight()*scale) / 2 );
         writeToTransform();
     }
-    
+
     public void fit() {
         Bounds gb = zoomGroup.getLayoutBounds();
         Bounds tb = getLayoutBounds();
         double scalex = tb.getWidth() / (gb.getWidth() + 10.0/scale.get());
         double scaley = tb.getHeight() / (gb.getHeight() + 70.0/scale.get());
-        
+
         double scale = Math.min(1.0, Math.min(scalex, scaley));
 
         tx.set((tb.getWidth() - gb.getWidth() * scale) / 2);
         ty.set((tb.getHeight() - gb.getHeight()*scale) / 2 + 25);
         this.scale.set(scale);
         writeToTransform();
-        
+
     }
-    
+
     public void zoomOneToOne() {
         Bounds tb = getLayoutBounds();
         double scale = 1 / this.scale.get();
-        
+
         zoom(scale, tb.getWidth() / 2, tb.getHeight() / 2);
     }
-    
+
     public void centerOnNode(Node n) {
         if (n == null) return;
-        
+
         Bounds b = n.getLayoutBounds();
         while (n != null && n != zoomGroup) {
             b = n.localToParent(b);
@@ -206,5 +214,5 @@ public class ZoomPane extends Pane {
             writeToTransform();
         }
     }
-    
+
 }
